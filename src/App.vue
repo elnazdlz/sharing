888e217df0e9d766d8694e7d08996828b4c721f9
elnaz <delazimi.elnaz@gmail.com>
2020-03-26 19:47:15 +0430

    logint not complited

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="1"/>
+  <RouterView />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
---
+++ src/components/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mainLogin">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <ElForm
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+    >
+      <span class="signInStyle">Sing In</span>
+      <el-input placeholder="Please input" v-model="email"></el-input>
+      <el-input placeholder="Please input password" v-model="password" show-password></el-input>
+    </ElForm>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      passwrod: ""
+    };
+  }
+};
+</script>
+
+
+<style lang="scss">
+@import "./style.scss";
+</style>
